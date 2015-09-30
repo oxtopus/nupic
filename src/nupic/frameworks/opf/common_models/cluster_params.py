6b341a1828213237e9e425b409b3f8f20baf6804
Austin Marshall <amarshall@numenta.com>
2015-09-30 09:17:12 -0700

    Fixup style-guide violations

--- src/nupic/frameworks/opf/common_models/cluster_params.py
+++ src/nupic/frameworks/opf/common_models/cluster_params.py
@@ -31,16 +31,17 @@ def setRandomEncoderParams(params, minVal, maxVal, minResolution):
   Given model params, figure out the correct parameters for the
   RandomDistributed encoder. Modifies params in place.
   """
-  encodersDict= (
-    params['modelConfig']['modelParams']['sensorParams']['encoders']
-    )
-  for v in encodersDict.itervalues():
-    if v is not None:
-      if v['type'] == 'RandomDistributedScalarEncoder':
+  encodersDict = (
+    params["modelConfig"]["modelParams"]["sensorParams"]["encoders"]
+  )
+
+  for encoder in encodersDict.itervalues():
+    if encoder:
+      if encoder["type"] == "RandomDistributedScalarEncoder":
         resolution = max(minResolution,
-                         (maxVal - minVal) / v.pop('numBuckets')
+                         (maxVal - minVal) / encoder.pop("numBuckets")
                         )
-        encodersDict['c1']['resolution'] = resolution
+        encodersDict["c1"]["resolution"] = resolution
 
 
 
@@ -53,22 +54,20 @@ def getScalarMetricWithTimeOfDayParams(metricData,
     Return an ordered list of JSON strings that can be used to create an
     anomaly model for OPF's ModelFactory.
 
+    :param metricData: numpy array of metric data. Used to calculate minVal
+      and maxVal if either is unspecified
 
-    Args:
-        metricData:  numpy array of metric data. Used to calculate minVal
-                     and maxVal if either is unspecified
+    :param minVal: minimum value of metric. Used to set up encoders. If None
+      will be derived from metricData.
 
-        minVal:      minimum value of metric. Used to set up encoders. If None
-                     will be derived from metricData.
+    :param maxVal: maximum value of metric. Used to set up input encoders. If
+      None will be derived from metricData
 
-        maxVal:      maximum value of metric. Used to set up input encoders. If
-                     None will be derived from metricData
+    :param minResolution: minimum resolution of metric. Used to set up
+      encoders.  If None, will use default value of 0.001.
 
-        minResolution: minimum resolution of metric. Used to set up encoders.
-                       If None, will use default value of 0.001.
-
-        useRandomEncoder: if True, use RandomDistributedScalarEncoder instead
-                     of ScalarEncoder
+    :param useRandomEncoder: if True, use RandomDistributedScalarEncoder
+      instead of ScalarEncoder
 
     Assumptions:
         The timeStamp field corresponds to c0
@@ -80,17 +79,17 @@ def getScalarMetricWithTimeOfDayParams(metricData,
 
   # Read a sorted list of parameters from the appropriate directory
   if useRandomEncoder:
-    paramsDirectory = 'anomaly_params_random_encoder'
+    paramsDirectory = "anomaly_params_random_encoder"
   else:
-    # This will go away soon MER-1442
-    paramsDirectory = 'scalarMetricWithTimeOfDayAnomalyParams'
+    paramsDirectory = "scalarMetricWithTimeOfDayAnomalyParams"
 
-  with resource_stream('nupic.frameworks.opf.common_models',
-                       os.path.join(paramsDirectory,
-                                    'paramOrder.csv')) as fileObj:
-    paramFiles = csv.reader(fileObj).next()
+  with resource_stream(
+        "nupic.frameworks.opf.common_models",
+        os.path.join(paramsDirectory, "paramOrder.csv")
+      ) as fileObj:
+    paramFiles = next(csv.reader(fileObj))
 
-  paramSets=[]
+  paramSets = []
 
   # Compute min and/or max from the data if not specified
   if minVal is None or maxVal is None:
@@ -106,20 +105,20 @@ def getScalarMetricWithTimeOfDayParams(metricData,
 
   # Iterate over the parameters for each model and replace the appropriate
   # min/max values to the computed ones.
-  for p in paramFiles:
-    with resource_stream('nupic.frameworks.opf.common_models',
-                         os.path.join(paramsDirectory, p)) as infile:
-      paramSet=json.load(infile)
+  for filename in paramFiles:
+    with resource_stream("nupic.frameworks.opf.common_models",
+                         os.path.join(paramsDirectory, filename)) as infile:
+      paramSet = json.load(infile)
 
     encodersDict= (
-      paramSet['modelConfig']['modelParams']['sensorParams']['encoders'])
+      paramSet["modelConfig"]["modelParams"]["sensorParams"]["encoders"]
+    )
 
     if useRandomEncoder:
       setRandomEncoderParams(paramSet, minVal, maxVal, minResolution)
     else:
-      # TODO Update per MER-1442
-      encodersDict['c1']['maxval'] = maxVal
-      encodersDict['c1']['minval'] = minVal
+      encodersDict["c1"]["maxval"] = maxVal
+      encodersDict["c1"]["minval"] = minVal
 
     paramSets.append(paramSet)
 
@@ -132,8 +131,8 @@ def rangeGen(data, std=1):
   Return reasonable min/max values to use given the data.
   """
   dataStd = np.std(data)
-  if dataStd==0:
-    dataStd=1
+  if dataStd == 0:
+    dataStd = 1
   minval = np.min(data) -  std * dataStd
   maxval = np.max(data) +  std * dataStd
   return minval, maxval
