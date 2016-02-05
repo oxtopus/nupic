7b2e1b4d968fdb0783d15307a004f92617b26c4f
Austin Marshall <amarshall@numenta.com>
2016-02-05 11:28:30 -0800

    bamboo entry point for linux

---
+++ ci/bamboo/build-linux.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -o errexit
+set -o xtrace
+apt-get update
+apt-get install -y \
+    curl \
+    wget \
+    git-core \
+    gcc \
+    g++ \
+    cmake \
+    python \
+    python2.7 \
+    python2.7-dev \
+    zlib1g-dev \
+    bzip2 \
+    libyaml-dev \
+    libyaml-0-2
+wget https://bootstrap.pypa.io/get-pip.py -O - | python
+pip install --upgrade setuptools
+pip install wheel
+export CC=gcc
+export CXX=g++
+pwd
+ls -laFh
