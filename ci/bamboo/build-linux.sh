7e192b37d21bedd764ba115cfb98bf68929c4a07
Austin Marshall <amarshall@numenta.com>
2016-02-09 13:47:32 -0800

    Parameterized compiler

--- ci/bamboo/build-linux.sh
+++ ci/bamboo/build-linux.sh
@@ -1,25 +1,46 @@
 #!/bin/bash
 set -o errexit
 set -o xtrace
+
+# Environment defaults w/ defaults
+if [ -z "${USER}" ]; then
+    USER="docker"
+fi
+export USER
+
+# Setup compiler
+if [ -z "${CC}" ]; then
+    CC="gcc"
+fi
+export CC
+
+if [ "${CC}" = "clang" ]; then
+    if [ -z "${CXX}" ]; then
+        CXX="clang++"
+    fi
+    COMPILER_PACKAGES="clang-3.4" # Ubuntu-specific apt package name
+else
+    if [ -z "${CXX}" ]; then
+        CXX="g++"
+    fi
+    COMPILER_PACKAGES="${CC} ${CXX}" # Ubuntu-specific apt package names
+fi
+export CXX
+
+# Install OS dependencies, assuming stock ubuntu:latest
 apt-get update
 apt-get install -y \
-    curl \
     wget \
-    git-core \
-    gcc \
-    g++ \
     cmake \
     python \
     python2.7 \
-    python2.7-dev \
-    zlib1g-dev \
-    bzip2 \
-    libyaml-dev \
-    libyaml-0-2
+    python2.7-dev
 wget https://bootstrap.pypa.io/get-pip.py -O - | python
 pip install --upgrade setuptools
 pip install wheel
-export CC=gcc
-export CXX=g++
-pip install nupic.bindings-0.3.2.dev0-cp27-cp27mu-linux_x86_64.whl
-python setup.py bdist bdist_dumb bdist_wheel sdist install
+
+# Install artifacts from previous build
+pip install nupic.bindings-*-cp27-cp27mu-linux_x86_64.whl
+
+# Build installable python packages
+python setup.py bdist bdist_dumb bdist_wheel sdist
